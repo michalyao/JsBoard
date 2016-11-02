2f7e3f2511e718408d9ea6b89ecb106635ca29cd
michalyao <yaoyao0777@Gmail.com>
2016-11-02 20:27:54 +0800

    add model

---
+++ src/backbone/primer/index.js
@@ -0,0 +1,19 @@
+var KermitModel = Backbone.Model.extend({
+  url: '/muppets/1', // defines a restful url to sync wiht
+  defaulst: {   // default attributes
+    id: null,
+    name: null,
+    occupation: null
+  }
+});
+
+// extend a backbone component, and then get back a constructor function.
+// call fetch return a Promise Object
+var kermit = new KermitModel();
+// get request
+kermit.fetch().then(() => {
+  kermit.get('name');
+  kermit.get('occupation');
+  kermit.set('occupation', 'muppet ringleader');
+  kermit.save();
+});
