e026219b96be75578afdfa6b3416609aabf39457
Yao <yaoyao0777@gmail.com>
2017-03-18 00:09:36 +0800

    class and module test.

---
+++ src/es6/class-module/calculator.js
@@ -0,0 +1,9 @@
+export class Calculator {
+    constructor() {}
+    sum(x, y) {
+        return x + y;
+    }
+    static sub(x, y) {
+        return x - y;
+    }
+}
---
+++ src/es6/class-module/test.js
@@ -0,0 +1,6 @@
+import { Calculator } from "./calculator.js";
+let calculator = new Calculator();
+var sum = calculator.sum(1, 2);
+console.log(sum);
+var sub = Calculator.sub(2, 1);
+console.log(sub)
